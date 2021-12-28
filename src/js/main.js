99ccf1b89ff7af356d5b13446aef97554885026f
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-12-28 11:04:49 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,8 @@ $("button").on("click", function () {
 	$("div").eq(2).toggleClass("active");
 });
 
-console.log($("button").html())
-
-// console.log($(".active").index())
-
 $("div").click(function () {
-	// console.log($(this));
+	console.log($(this));
 });
 
 // console.log($("div").eq(2).find(".some"));
